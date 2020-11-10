3143ab9ec6cb0d1ff070bcc4f5234f6dda0deded
Nishit Verma <nishitverma1312@gmail.com>
2020-11-11 01:25:38 +0530

    Update and rename Token1.java to Token.java

--- Token.java
+++ Token.java
@@ -1,11 +1,11 @@
-package ir.token1.lab;
+package ir.token.lab;
 
 import java.util.*;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-public class Token1 
+public class Token
 {
 	public static void main(String []args)
 	{
